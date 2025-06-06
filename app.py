a8ef36dcf1a331033ccc3109dc778110a983b824
KsuPl <Platoshka_ks@mail.ru>
2025-06-06 18:59:17 +0700

    Подключила бд

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:hI3DIvF0YsymTSB2@db.vqcizotnverfqmmozxjx.supabase.co:5432/postgres'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #lcl
 
 db = SQLAlchemy(app)
 @app.route('/')
