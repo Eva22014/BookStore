be433f1d54ac03f4652ce7bb3211c9d786af0bed
KsuPl <Platoshka_ks@mail.ru>
2025-06-06 18:57:41 +0700

    Подключила бд

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:hI3DIvF0YsymTSB2@db.vqcizotnverfqmmozxjx.supabase.co:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return "Hello, Ksushka!"
