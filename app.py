2d581c366656b5b4a0c93ce7ab1c5884b6549d64
Eva22014 <bataeeva@bk.ru>
2025-06-08 23:11:55 +0700

    Добавлено новое изменение

--- app.py
+++ app.py
@@ -148,3 +148,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создает только новые таблицы, если они не существуют
     app.run(debug=True, port=5001)
+
+# Тестовое изменение для проверки Git, 08.06.2025
