6f24b382ebb24ee62f82223dba0c33826c53ebad
Eva22014 <bataeeva@bk.ru>
2025-06-10 15:58:59 +0700

    njnjk

--- app.py
+++ app.py
@@ -813,3 +813,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создает только новые таблицы, если они не существуют
     app.run(debug=True, port=5001)
+
+    #kkfkfkf
